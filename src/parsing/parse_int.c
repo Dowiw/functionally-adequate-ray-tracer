2bc6432719a61e0a1334c5a94712334a75e63ec3
simeonstark <vierzehnmalfuenf@gmail.com>
2026-06-15 21:35:12 +0200

    add: implement int and double parsing

--- src/parsing/parse_int.c
+++ src/parsing/parse_int.c
@@ -6,14 +6,41 @@
 /*   By: sstark <sstark@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 21:22:03 by sstark            #+#    #+#             */
-/*   Updated: 2026/06/09 22:22:41 by sstark           ###   ########.fr       */
+/*   Updated: 2026/06/15 21:26:04 by sstark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// TODO
+#include "../libft/libft.h"
+
+/*
+ * Parses the given string 'str' as an integer.
+ *  - Does not handle integer over- or underflow.
+ *  - Does not allow whitespace.
+ *  - Allows one sign character, either '+' or '-'.
+ *  - Allows leading zeros.
+ * The result is stored in the 'num' pointer.
+ * Returns true if the parsing was successful.
+ */
 int	parse_int(int *num, char *str)
 {
-	(void) str;
+	int	i;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!ft_isdigit(str[i]))
+		return (0);
 	*num = 0;
+	while (str[i] != '\0')
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		*num = *num * 10 + (str[i] - '0') * sign;
+		i++;
+	}
 	return (1);
 }
