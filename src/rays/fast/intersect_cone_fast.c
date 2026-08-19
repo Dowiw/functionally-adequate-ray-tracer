304bed24eaf4cd40bcd23eed8e79619855f7f433
monjie12272@gmail.com <monjie12272@gmail.com>
2026-08-19 15:55:13 +0200

    add: ability to use transform matrix for cylinder and cones

--- src/rays/fast/intersect_cone_fast.c
+++ src/rays/fast/intersect_cone_fast.c
@@ -45,6 +45,34 @@ static inline void		intersect_caps_fast(t_cone *cone, t_ray r, t_intersection *h
 		*hit = (t_intersection){t, (t_object){CONE, cone}};
 }
 
+static inline t_tuple	inline_matrix4x4_multiply_tuple(t_matrix4x4 a, t_tuple b)
+{
+	t_tuple	result;
+
+	result.x = a.m[0][0] * b.x
+		+ a.m[0][1] * b.y
+		+ a.m[0][2] * b.z
+		+ a.m[0][3] * b.w;
+	result.y = a.m[1][0] * b.x
+		+ a.m[1][1] * b.y
+		+ a.m[1][2] * b.z
+		+ a.m[1][3] * b.w;
+	result.z = a.m[2][0] * b.x
+		+ a.m[2][1] * b.y
+		+ a.m[2][2] * b.z
+		+ a.m[2][3] * b.w;
+	result.w = a.m[3][0] * b.x
+		+ a.m[3][1] * b.y
+		+ a.m[3][2] * b.z
+		+ a.m[3][3] * b.w;
+	return (result);
+};
+
+static inline t_ray		inline_transform(t_ray r, t_matrix4x4 m)
+{
+	return ((t_ray){inline_matrix4x4_multiply_tuple(m, r.origin), inline_matrix4x4_multiply_tuple(m, r.direction)});
+}
+
 void	intersect_cone_fast(t_cone *cone, t_ray ray, t_intersection *hit)
 {
 	double			abc[3];
@@ -52,6 +80,7 @@ void	intersect_cone_fast(t_cone *cone, t_ray ray, t_intersection *hit)
 	double			t;
 	double			y;
 
+	ray = inline_transform(ray, cone->transform);
 	abc[0] = pow2(ray.direction.x) - pow2(ray.direction.y)
 		+ pow2(ray.direction.z);
 	abc[1] = (2 * ray.origin.x * ray.direction.x)
